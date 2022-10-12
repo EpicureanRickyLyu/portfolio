e40ce3109ee4fb4e8a2e592632ca3e586c549e8d
Yaaaatingbabe <lyuqiricky@gmail.com>
2022-10-12 19:36:14 +0100

    unrealRPG

--- src/views/TableContent/Unreal.component.jsx
+++ src/views/TableContent/Unreal.component.jsx
@@ -24,6 +24,18 @@ export default function Unreal() {
                     </div>
                   
                 </div>
+                <div onClick={ClickAlert} className="w-full lg:w-6/12 px-4">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    UNREAL RPG
+                  </h5>
+                  
+                    <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                    <video autoPlay muted loop  className="align-middle border-none max-w-full h-auto rounded-lg">
+                    <source src={require("assets/video/UnrealRPG.mp4").default} type="video/mp4"></source>
+                    </video>
+                    </div>
+                  
+                </div>
 
                 <div onClick={ClickAlert} className="w-full lg:w-4/12 px-4">
                   <h5 className="text-xl font-semibold pb-4 text-center">
